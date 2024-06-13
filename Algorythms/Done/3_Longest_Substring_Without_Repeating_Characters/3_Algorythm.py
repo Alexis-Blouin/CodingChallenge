53fe5848930f7914c343598a471cd6411244781a
AlexisBlouin <blouin.alexis@icloud.com>
2024-06-13 19:02:07 -0400

    Trying problem 6 and adding success rate to the done ones

---
+++ Algorythms/Done/3_Longest_Substring_Without_Repeating_Characters/3_Algorythm.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        longest = ""
+        for i in range(len(s)):
+            result = ""
+            for char in s[i:]:
+                if char in result:
+                    break
+                result += char
+            if len(result) > len(longest):
+                longest = result
+        return len(longest)
+
+
+if __name__ == "__main__":
+    print(Solution().lengthOfLongestSubstring("abcabcbb"))
+    print(Solution().lengthOfLongestSubstring("bbbbb"))
+    print(Solution().lengthOfLongestSubstring("pwwkew"))
