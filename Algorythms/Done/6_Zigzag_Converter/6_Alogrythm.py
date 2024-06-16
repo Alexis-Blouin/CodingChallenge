304a41987cbb7e0a240ee918493dbf5d1f924d40
AlexisBlouin <blouin.alexis@icloud.com>
2024-06-16 01:35:53 -0400

    Zigzag conversion completed

---
+++ Algorythms/Done/6_Zigzag_Converter/6_Alogrythm.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1:
+            return s
+        if numRows == 2:
+            return s[::2] + s[1::2]
+
+        numIterations = 2 * numRows - 2
+        i = 0
+        result = ""
+        while i < numRows:
+            if i == 0 or i == numRows - 1:
+                j = 0
+                while j + i < len(s):
+                    result += s[j + i]
+                    j += numIterations
+            else:
+                j = 0
+                while j + i < len(s):
+                    result += s[j + i]
+                    if j + numIterations - i < len(s):
+                        result += s[j + numIterations - i]
+                    j += numIterations
+            i += 1
+
+        return result
+
+
+if __name__ == "__main__":
+    # Expected output: "PAHNAPLSIIGYIR"
+    print(Solution().convert("PAYPALISHIRING", 3))
+    # Expected output: "PINALSIGYAHRPI"
+    print(Solution().convert("PAYPALISHIRING", 4))
+    # Expected output: "A"
+    print(Solution().convert("A", 1))
