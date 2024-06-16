3d43e452e7ffffad00e2d3311b587f4462bed7c2
AlexisBlouin <blouin.alexis@icloud.com>
2024-06-16 01:48:18 -0400

    Added problem 7 and 8. Added constraints to all problems.

---
+++ Algorythms/Attempted/7_Reverse_Integer/7_Algorythm.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+
+
+if __name__ == "__main__":
+    # Expected output: 321
+    print(Solution().reverse(123))
+    # Expected output: -321
+    print(Solution().reverse(-123))
+    # Expected output: 21
+    print(Solution().reverse(120))
