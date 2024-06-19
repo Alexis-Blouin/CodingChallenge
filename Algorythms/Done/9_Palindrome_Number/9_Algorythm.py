9e6c6850dbc6a17c8c39347474c2e5e289842b67
AlexisBlouin <blouin.alexis@icloud.com>
2024-06-19 18:20:46 -0400

    9 is done, 10 under progress

--- Algorythms/Done/9_Palindrome_Number/9_Algorythm.py
+++ Algorythms/Done/9_Palindrome_Number/9_Algorythm.py
@@ -4,6 +4,8 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
+        str_x = str(x)
+        return str_x == str_x[::-1]
 
 
 if __name__ == "__main__":
--- Algorythms/Under_Progress/10_Regular_Expression_Matching/10_Algorythm.py
+++ Algorythms/Under_Progress/10_Regular_Expression_Matching/10_Algorythm.py
@@ -5,6 +5,40 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
+        ii = 0
+        for i in range(len(s)):
+            if ii < len(p):
+                if s[i] != p[ii] and p[ii] != "." and p[ii] != "*":
+                    while ii < len(p) and s[i] not in [p[ii], ".", "*"]:
+                        ii += 1
+                    if ii == len(p):
+                        print("1")
+                        return False
+                elif s[i] == p[ii] or p[ii] == ".":
+                    ii += 1
+                elif p[ii] == "*":
+                    ii += 1
+                    while ii < len(p) and p[ii] == ".":
+                        ii += 1
+                        i += 1
+                    if ii == len(p) and i < len(s):
+                        return True
+                    elif ii == 0:
+                        print("2")
+                        return False
+                    print(s[i], p[ii])
+                    while i < len(s) and s[i] != p[ii]:
+                        i += 1
+                    if i == len(s):
+                        print("3")
+                        return False
+            else:
+                print("4")
+                return False
+        print(ii)
+        if ii < len(p) and p[ii] != "*" and p[ii] != ".":
+            return False
+        return True
 
 
 if __name__ == "__main__":
@@ -15,3 +49,5 @@ if __name__ == "__main__":
     print(sol.isMatch("aa", "a*"))
     # Expected result: True
     print(sol.isMatch("ab", ".*"))
+    # Expected result: True
+    print(sol.isMatch("aab", "c*a*b"))  # Fails
