25a992fc53874a4689526e15b8b5d26b372331b3
AlexisBlouin <blouin.alexis@icloud.com>
2024-06-25 21:25:12 -0400

    Number 10 is still not working

--- Algorythms/Under_Progress/10_Regular_Expression_Matching/10_Algorythm.py
+++ Algorythms/Under_Progress/10_Regular_Expression_Matching/10_Algorythm.py
@@ -6,48 +6,50 @@ class Solution(object):
         :rtype: bool
         """
         ii = 0
-        for i in range(len(s)):
-            if ii < len(p):
-                if s[i] != p[ii] and p[ii] != "." and p[ii] != "*":
-                    while ii < len(p) and s[i] not in [p[ii], ".", "*"]:
-                        ii += 1
-                    if ii == len(p):
-                        print("1")
-                        return False
-                elif s[i] == p[ii] or p[ii] == ".":
-                    ii += 1
-                elif p[ii] == "*":
-                    ii += 1
-                    while ii < len(p) and p[ii] == ".":
-                        ii += 1
-                        i += 1
-                    if ii == len(p) and i < len(s):
-                        return True
-                    elif ii == 0:
-                        print("2")
-                        return False
-                    print(s[i], p[ii])
-                    while i < len(s) and s[i] != p[ii]:
-                        i += 1
-                    if i == len(s):
-                        print("3")
-                        return False
-            else:
-                print("4")
+        for i in range(len(p)):
+            while i < len(p) and p[i] != s[ii] and p[i] != "." and p[i] != "*":
+                i += 1
+            if p[i] != s[ii] and p[i] != "." and p[i] != "*":
                 return False
-        print(ii)
-        if ii < len(p) and p[ii] != "*" and p[ii] != ".":
-            return False
-        return True
+            if p[i] == ".":
+                i += 1
+                continue
+            elif p[i] == "*":
+                if i == len(p) - 1 and ii == len(s) - 1:
+                    return True
+                while ii < len(s) and s[ii] == p[i - 1]:
+                    print(s[ii], p[i - 1])
+                    ii += 1
+                if ii == len(s) and i < len(p) - 1:
+                    return False
+                if p[i + 1] == ".":
+                    i += 1
+                    if i == len(p) - 1:
+                        if ii == len(s) - 1:
+                            return True
+                        else:
+                            return False
+            else:
+                ii += 1
+
+        if ii == len(s):
+            return True
+        return False
 
 
 if __name__ == "__main__":
     sol = Solution()
     # Expected result: False
-    print(sol.isMatch("aa", "a"))
+    # print(sol.isMatch("aa", "a"))
     # Expected result: True
-    print(sol.isMatch("aa", "a*"))
+    # print(sol.isMatch("aa", "a*"))
     # Expected result: True
-    print(sol.isMatch("ab", ".*"))
+    # print(sol.isMatch("ab", ".*"))
     # Expected result: True
-    print(sol.isMatch("aab", "c*a*b"))  # Fails
+    # print(sol.isMatch("aab", "c*a*b"))
+    # Expected result: False
+    print(sol.isMatch("mississippi", "mis*is*p*."))
+    # Expected result: True
+    print(sol.isMatch("mississippi", "mis*is*ip*."))
+    # Expected result: False
+    print(sol.isMatch("abcd", "d*"))
