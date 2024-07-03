28bcf97c48f612777b05437008f89ff7f36cc5ec
AlexisBlouin <blouin.alexis@icloud.com>
2024-07-03 00:01:51 -0400

    Completed #12. Still working on #10.

--- Algorythms/Under_Progress/10_Regular_Expression_Matching/10_Algorythm.py
+++ Algorythms/Under_Progress/10_Regular_Expression_Matching/10_Algorythm.py
@@ -5,47 +5,67 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
-        ii = 0
-        for i in range(len(p)):
-            while i < len(p) and p[i] != s[ii] and p[i] != "." and p[i] != "*":
-                i += 1
-            if p[i] != s[ii] and p[i] != "." and p[i] != "*":
-                return False
-            if p[i] == ".":
-                i += 1
-                continue
-            elif p[i] == "*":
-                if i == len(p) - 1 and ii == len(s) - 1:
-                    return True
-                while ii < len(s) and s[ii] == p[i - 1]:
-                    print(s[ii], p[i - 1])
-                    ii += 1
-                if ii == len(s) and i < len(p) - 1:
-                    return False
-                if p[i + 1] == ".":
-                    i += 1
-                    if i == len(p) - 1:
-                        if ii == len(s) - 1:
-                            return True
-                        else:
-                            return False
-            else:
-                ii += 1
 
-        if ii == len(s):
-            return True
-        return False
+        # last_c = ""
+        last_cc = ""
+        for i in range(len(s)):
+            if p == "":
+                return False
+            for cc in p:
+                print(s[i], cc, p)
+                if s[i] == cc:
+                    p = p[p.index(cc) + 1 :]
+                    last_cc = cc
+                    break
+                elif cc == ".":
+                    p = p[p.index(cc) + 1 :]
+                    last_cc = cc
+                    break
+                elif cc == "*":
+                    if last_cc == ".":
+                        return True
+                    if last_cc != "":
+                        while p != "" and s[i] == last_cc:
+                            i += 1
+                        p = p[p.index(cc) + 1 :]
+                        break
+
+                        # for c in p:
+                        #     if s == "":
+                        #         return True
+                        #     for cc in s:
+                        #         print(c, cc, s)
+                        #         if c == cc:
+                        #             s = s[s.index(cc) + 1 :]
+                        #             last_c = c
+                        #             break
+                        #         elif c == ".":
+                        #             s = s[s.index(cc) + 1 :]
+                        #             last_c = c
+                        #             break
+                        #         elif c == "*":
+                        #             if last_c == ".":
+                        #                 return True
+                        #             if last_c != "":
+                        #                 # print(last_c, s)
+                        #                 while s != "" and s[0] == last_c:
+                        #                     s = s[s.index(s[0]) + 1 :]
+                        # break
+            if last_cc == cc:
+                return False
+
+        return True
 
 
 if __name__ == "__main__":
     sol = Solution()
-    # Expected result: False
+    # # Expected result: False
     # print(sol.isMatch("aa", "a"))
-    # Expected result: True
+    # # Expected result: True
     # print(sol.isMatch("aa", "a*"))
-    # Expected result: True
+    # # Expected result: True
     # print(sol.isMatch("ab", ".*"))
-    # Expected result: True
+    # # Expected result: True
     # print(sol.isMatch("aab", "c*a*b"))
     # Expected result: False
     print(sol.isMatch("mississippi", "mis*is*p*."))
--- Algorythms/Under_Progress/12_Integer_to_Roman/12_Algorythm.py
+++ Algorythms/Under_Progress/12_Integer_to_Roman/12_Algorythm.py
@@ -4,6 +4,54 @@ class Solution(object):
         :type num: int
         :rtype: str
         """
+        s = ""
+        res = num % 1000
+        s += int((num - res) / 1000) * "M"
+        num = res
+
+        if len(str(num)) == 3:
+            if str(num)[0] == "9":
+                s += "CM"
+                num -= 900
+            elif str(num)[0] == "4":
+                s += "CD"
+                num -= 400
+            else:
+                res = num % 500
+                s += int((num - res) / 500) * "D"
+                num = res
+                res = num % 100
+                print(res, num)
+                s += int((num - res) / 100) * "C"
+                num = res
+
+        if len(str(num)) == 2:
+            if str(num)[0] == "9":
+                s += "XC"
+                num -= 90
+            elif str(num)[0] == "4":
+                s += "XL"
+                num -= 40
+            else:
+                res = num % 50
+                s += int((num - res) / 50) * "L"
+                num = res
+                res = num % 10
+                s += int((num - res) / 10) * "X"
+                num = res
+
+        if str(num)[0] == "9":
+            s += "IX"
+            num -= 9
+        elif str(num)[0] == "4":
+            s += "IV"
+            num -= 4
+        else:
+            res = num % 5
+            s += int((num - res) / 5) * "V"
+            num = res
+            s += num * "I"
+        return s
 
 
 if __name__ == "__main__":
@@ -14,3 +62,5 @@ if __name__ == "__main__":
     print(sol.intToRoman(58))
     # Expected result: "MCMXCIV"
     print(sol.intToRoman(1994))
+    # Expected result: "IV"
+    print(sol.intToRoman(4))
