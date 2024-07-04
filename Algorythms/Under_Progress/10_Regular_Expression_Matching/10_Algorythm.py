194bfa4ca82e34d242ab1c6cf760cfed2b9dc34a
AlexisBlouin <blouin.alexis@icloud.com>
2024-07-03 21:14:34 -0400

    Still wokring on the num 10, I just relocated the 12 to the finished ones

--- Algorythms/Under_Progress/10_Regular_Expression_Matching/10_Algorythm.py
+++ Algorythms/Under_Progress/10_Regular_Expression_Matching/10_Algorythm.py
@@ -5,14 +5,13 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
-
-        # last_c = ""
         last_cc = ""
         for i in range(len(s)):
             if p == "":
+                print("fales0")
                 return False
             for cc in p:
-                print(s[i], cc, p)
+                print(cc, p, s[i])
                 if s[i] == cc:
                     p = p[p.index(cc) + 1 :]
                     last_cc = cc
@@ -23,41 +22,41 @@ class Solution(object):
                     break
                 elif cc == "*":
                     if last_cc == ".":
-                        return True
+                        if len(p) == 1:
+                            return True
+                        else:
+                            p = p[p.index(cc) + 1 :]
+                            last_cc = last_cc
+                            break
                     if last_cc != "":
-                        while p != "" and s[i] == last_cc:
+                        print("yo")
+                        print(p, cc, last_cc, s[i])
+                        while p != "" and i < len(s) and s[i] == last_cc:
+                            print("test")
                             i += 1
+                            p = p[p.index(cc) + 1 :]
                         p = p[p.index(cc) + 1 :]
+                        last_cc = cc
+                        i -= 1
+                        print(p, cc, last_cc, s[i])
+                        print("yo")
                         break
-
-                        # for c in p:
-                        #     if s == "":
-                        #         return True
-                        #     for cc in s:
-                        #         print(c, cc, s)
-                        #         if c == cc:
-                        #             s = s[s.index(cc) + 1 :]
-                        #             last_c = c
-                        #             break
-                        #         elif c == ".":
-                        #             s = s[s.index(cc) + 1 :]
-                        #             last_c = c
-                        #             break
-                        #         elif c == "*":
-                        #             if last_c == ".":
-                        #                 return True
-                        #             if last_c != "":
-                        #                 # print(last_c, s)
-                        #                 while s != "" and s[0] == last_c:
-                        #                     s = s[s.index(s[0]) + 1 :]
-                        # break
-            if last_cc == cc:
+                else:
+                    p = p[p.index(cc) + 1 :]
+                    last_cc = cc
+            if last_cc != cc:
+                print("fales1")
                 return False
 
+        if len(p) != 0:
+            print(p)
+            print("fales2")
+            return False
         return True
 
 
 if __name__ == "__main__":
+    # Works till #278
     sol = Solution()
     # # Expected result: False
     # print(sol.isMatch("aa", "a"))
@@ -65,11 +64,17 @@ if __name__ == "__main__":
     # print(sol.isMatch("aa", "a*"))
     # # Expected result: True
     # print(sol.isMatch("ab", ".*"))
-    # # Expected result: True
-    # print(sol.isMatch("aab", "c*a*b"))
-    # Expected result: False
-    print(sol.isMatch("mississippi", "mis*is*p*."))
     # Expected result: True
-    print(sol.isMatch("mississippi", "mis*is*ip*."))
+    print(sol.isMatch("aab", "c*a*b"))
+    # # Expected result: False
+    # print(sol.isMatch("mississippi", "mis*is*p*."))
+    # # Expected result: True
+    # print(sol.isMatch("mississippi", "mis*is*ip*."))
+    # # Expected result: False
+    # print(sol.isMatch("abcd", "d*"))
+    # # Expected result: False
+    # print(sol.isMatch("ab", ".*c"))
+    # # Expected result: False
+    # print(sol.isMatch("aaa", "aaaa"))
     # Expected result: False
-    print(sol.isMatch("abcd", "d*"))
+    print(sol.isMatch("aaa", "ab*a"))
